1cd185982dde7eedaaa8999eeb8b7dba4667ebda
julianbarbieri1 <julian.barbieri@nybblegroup.com>
2022-11-11 02:00:11 -0300

    Commit fin dia 5 (proyecto finalizado)

--- ProjectAutomation/src/test/java/cleanTest/TestBaseTickTick.java
+++ ProjectAutomation/src/test/java/cleanTest/TestBaseTickTick.java
@@ -42,13 +42,10 @@ public class TestBaseTickTick {
 
             // generate a random number between
             // 0 to AlphaNumericString variable length
-            int index
-                    = (int)(AlphaNumericString.length()
-                    * Math.random());
+            int index = (int)(AlphaNumericString.length() * Math.random());
 
             // add Character one by one in end of sb
-            sb.append(AlphaNumericString
-                    .charAt(index));
+            sb.append(AlphaNumericString.charAt(index));
         }
 
         return sb.toString();
